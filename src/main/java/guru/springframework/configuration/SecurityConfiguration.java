ab4b07532989977501f60b0f53838823bb768adc
Prashant Singh <prashant3jan@gmail.com>
2023-06-02 16:32:06 +0530

    added login page and applied spring security

--- src/main/java/guru/springframework/configuration/SecurityConfiguration.java
+++ src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -1,19 +1,56 @@
 package guru.springframework.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/products","/product/**","/product/edit/**","/console/*","/h2-console/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/index", "/product").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index", true)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN");
     }
 
 }
